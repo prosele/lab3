14e328fe2bda03df4d2e22bb3714ae84980b2897
Kseniia Prokopovich <prokopovichksenia@gmail.com>
2023-10-08 14:56:09 +0300

    сщь1

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,14 +1,20 @@
-//
-//  main.cpp
-//  lab3
-//
-//  Created by Ксения Прокопович on 08.10.2023.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+int res(int i, int n, int *mass, int sum1, int sum2) {
+    if (i==n) {
+        return abs(sum1-sum2);
+    }
+    return min(res(i+1,n,mass,sum1+mass[i],sum2),res(i+1,n,mass,sum1,sum2+mass[i]));
+}
+int main() {
+    int n;
+    cin >> n;
+    int mass[n];
+    for (int i=0;i<n;i++) {
+        int x;
+        cin >> x;
+        mass[i]=x;
+    }
+    cout << res(0,n,mass,0,0);
+    
 }
